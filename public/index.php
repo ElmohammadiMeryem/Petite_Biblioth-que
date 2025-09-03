5e8c727c5bccaa98826c9ee657d0c8804114f099
Meryem <elmohammadimeryem@gmail.com>
2025-09-03 17:47:00 +0100

    test 3

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['_method ']) && $_POST[
     if (isset($_POST['id'])) {
 
 
-        
+        // test
         $id = (int)$_POST['id'];
         if ($bookModel->delete($id)) {
             $_SESSION['flash_message'] = 'Livre supprimé avec succés!';
